1e34d6c81184c9ac13e37ae583cb69cb7c9b7750
HAM <mcclellan.haley@gmail.com>
2020-09-22 16:14:26 -0500

    remove brittany's resume

--- src/components/nav.js
+++ src/components/nav.js
@@ -191,11 +191,7 @@ const Nav = ({ isHome }) => {
           <TransitionGroup component={null}>
             {isMounted && (
               <CSSTransition classNames={fadeDownClass} timeout={timeout}>
-                <div style={{ transitionDelay: `${isHome ? navLinks.length * 100 : 0}ms` }}>
-                  <a href="/resume.pdf" className="resume-button">
-                    Resume
-                  </a>
-                </div>
+                <div style={{ transitionDelay: `${isHome ? navLinks.length * 100 : 0}ms` }}></div>
               </CSSTransition>
             )}
           </TransitionGroup>
